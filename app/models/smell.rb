0389656006234fd3cac3360e37a5d9be1753760e
Ricky Chilcott <ricky@rbsavvy.com>
2013-02-21 23:54:49 -0500

    Default sort on reactions via name

--- app/models/smell.rb
+++ app/models/smell.rb
@@ -6,4 +6,8 @@ class Smell < ActiveRecord::Base
   validates_presence_of :name,  :lat, :lon, :up_votes, :down_votes
   
   geocoded_by :address, :latitude  => :lat, :longitude => :lon
+  
+  def reaction_ids
+    reactions.collect &:id
+  end
 end
