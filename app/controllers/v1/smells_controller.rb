c6e35c098f360d350a1583fcfe0b592b3887e17c
Ricky Chilcott <ricky@rbsavvy.com>
2013-02-22 08:00:33 -0500

    Adding reaction_ids to each smell

--- app/controllers/v1/smells_controller.rb
+++ app/controllers/v1/smells_controller.rb
@@ -12,24 +12,24 @@ class V1::SmellsController < ApplicationController
       smells = smells.near([params[:lat],params[:lon]], params[:dist] || 5)
     end
 
-    render json: smells.to_json
+    render json: smells.to_json(:methods => :reaction_ids)
   end
 
   def show
     smell = Smell.where(id: params[:id])
-    render json: smell.to_json
+    render json: smell.to_json(:methods => :reaction_ids)
   end
 
   def new
     smell = Smell.new
-    render json: smell.to_json
+    render json: smell.to_json(:methods => :reaction_ids)
   end
 
   def create
     smell = Smell.new(name: params[:name], lat: params[:lat], lon: params[:lon])
     
     if smell.save
-      render json: smell.to_json
+      render json: smell.to_json(:methods => :reaction_ids)
     else
       render json: Smell.new
     end
--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -4,4 +4,6 @@ class Reaction < ActiveRecord::Base
   has_and_belongs_to_many :smells
   
   validates_presence_of :name
+  
+  default_scope order(:name)
 end
